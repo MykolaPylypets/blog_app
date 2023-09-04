0b33e01708e8335dd551d74ac7a5747bcd253842
Mykola Pylypets <petrovychmykola@gmail.com>
2023-09-04 13:51:58 +0300

    1) Installed ejs 2) Modified routes 3) Renamed the pages folder to views. And changed the .html extensions    to .ejs

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const express = require('express')
 const app = new express()
 const path = require('path')
+const ejs = require('ejs')
 
 app.use(express.static('public'))
+
+app.set('view engine','ejs')
 
 app.listen(4000, ()=>{
   console.log('App listening on port 4000')
 })
 
-app.get('/',(req,res)=>{
-	res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+app.get('/', (req,res) => {
+  res.render('index');
 })
 
-app.get('/about',(req,res)=>{
-  res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+app.get('/about', (req,res) => {
+  res.render('about');
 })
 
-app.get('/post',(req,res)=>{
-  res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+app.get('/post', (req,res) => {
+  res.render('post');
 })
 
-app.get('/contact',(req,res)=>{
-  res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+app.get('/contact', (req,res) => {
+  res.render('contact');
 })
+
